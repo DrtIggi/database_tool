dce5b1d3579482533d3450216dbc68698590a3cb
Kharlamov <kharlamov@mpdl.mpg.de>
2025-02-14 15:02:37 +0100

    update check address in db

--- scripts/main.py
+++ scripts/main.py
@@ -4,13 +4,13 @@ import re
 from init_db import init_db
 from config import sqlite_db, input_csv, output_csv, logger, table_name, extract_address_parts, appartment_number_column_name
 
-def check_address_in_db(cursor, city, street, house, korpus, stroenie, appartment):
+def check_address_in_db(cursor, city, street, house, korpus,  type_of_street,appartment):
     cursor.execute(f'''
         SELECT * FROM {table_name} 
-        WHERE city = ? AND street = ? AND house = ? AND korpus = ? AND stroenie = ? AND "{appartment_number_column_name}" = ?
-    ''', (city, street, house, korpus, stroenie, appartment))
+        WHERE city = ? AND street = ? AND house = ? AND korpus = ?  AND type_of_street = ? AND {appartment_number_column_name} = ?
+    ''', (city, street, house, korpus,  type_of_street,appartment))
     
-    return cursor.fetchone()
+    return cursor.fetchall()
 
 
 def main():
@@ -26,9 +26,8 @@ def main():
         full_input_addr = extract_address_parts(f"{row['Город']},{row['Улица']},{row['Номер дома']},{row.get('Корпус', '')},{row.get('стр.', '')},{row.get('№ квартиры')}")
         if full_input_addr['street'] == '':
             continue
-        res = check_address_in_db(cursor, full_input_addr['city'],full_input_addr['street'],full_input_addr['house'],full_input_addr.get('korpus', ''),full_input_addr.get('stroenie', ''), full_input_addr.get('apartment', '')) 
-        if res:
-            row['data'] = res
+        res = check_address_in_db(cursor, full_input_addr['city'],full_input_addr['street'],full_input_addr['house'],full_input_addr.get('korpus', ''), full_input_addr.get('type_of_street'),full_input_addr.get('apartment', '')) 
+        row['data'] = str(res) if res else ''        
         
         updated_rows.append(row)  
 
